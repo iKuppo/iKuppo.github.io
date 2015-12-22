e0fe254ee5f09fc57d84bbb099bb21237148037f
Max Yeo <mzwyeo@gmail.com>
2015-12-21 19:04:18 -0500

    CNAME and new fonts

--- js/style.js
+++ js/style.js
@@ -28,18 +28,32 @@ $('#navicon').click(function() {
 
 // Backgorund Image Move with Mouse
 
+// var mouseX = 0, mouseY = 0;
+// $(document).mousemove(function(e){
+//    mouseX = e.pageX;
+//    mouseY = e.pageY; 
+// });
+
+// var loop = setInterval(function(){
+//     follower.css({left:xp, top:yp});
+// }, 30);
+
 $('.matrix-box').mousemove(function(e){
-    var midX = (e.offsetX - ($(this).width()/2))/-10;
-    var midY = (e.offsetY - ($(this).outerHeight()/2))/-10;
+	// var offset = $(this).offset();
+	// var relX = mouseX - offset.left;
+	// var relY = mouseY - offset.top;
+    var midX = (e.offsetX - ($(this).width()/2))/-20;
+    var midY = (e.offsetY - ($(this).outerHeight()/2))/-20;
     $(this).find('.matrix-image').css('left', midX);
     $(this).find('.matrix-image').css('top', midY);
 });
 
-// $('.matrix-box').mouseout(function(e){
-// 	console.log('hey');
-//     $(this).find('.matrix-image').css('left', 0);
-//     $(this).find('.matrix-image').css('top', 0);
-// });
+$('.matrix-box').mouseout(function(e){
+    $(this).find('.matrix-image').animate({
+    	top: 0,
+    	left: 0
+    }, 300);
+});
 
 $('.matrix-box h4').mousemove(function(e) {
 	e.stopPropagation();
