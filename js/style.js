7cb322589d1712377def8d28b621a7fdf9b8e465
Max Yeo <mzwyeo@gmail.com>
2015-12-22 13:07:51 -0500

    added styled contact page

--- js/style.js
+++ js/style.js
@@ -25,6 +25,8 @@ $('#navicon').click(function() {
 	$('header').toggleClass('open');
 });
 
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
 // Backgorund Image Move with Mouse
 
@@ -57,4 +59,47 @@ $('.matrix-box').mouseout(function(e){
 
 $('.matrix-box h4').mousemove(function(e) {
 	e.stopPropagation();
+});
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+// Contact
+
+function postToGoogle() {
+    var field1 = $('#name').val();
+    var field2 = $('#email').val();
+    var field3 = $('#message').val();
+
+    $.ajax({
+        url: "https://docs.google.com/forms/d/1U_r8MzNTkyPDFMAGCh0Y6Cvdbroja16Oh3kVr46wPw8/formResponse",
+        data: {
+        	"entry.1545498784": field1,
+        	"entry.1827286110": field2,
+        	"entry.976607894": field3
+        },
+        type: "POST",
+        dataType: "xml",
+        statusCode: {
+            0: function() {
+                clean();
+            },
+            200: function() {
+            	clean();
+            }
+        }
+    });
+}
+
+function clean() {
+	$('#name').val('');
+	$('#email').val('');
+	$('#message').val('');
+}
+
+$(document).ready(function(){
+    $('#contact-form').submit(function() {
+        postToGoogle();
+        return false;
+    });
 });
